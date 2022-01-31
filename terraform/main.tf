96b20eea69887c5de904bd08c30c460470fcbe2c
abdelrahmansaf <77361942+abdelrahmansaf@users.noreply.github.com>
2022-01-31 10:53:44 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "tform-sabdulrahman"
+        storage_account_name = "suckitterra"
+        container_name       = "myfuckingcontainer"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
+
+
+provider "azurerm" {
+  version = ">=2.0"
+  features {}
+}
+
+
+# resource "azurerm_resource_group" "rg" {
+#     name = "tform-sabdulrahmans"
+#     location = "westeurope"
+# }
+
+
+
+
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "sosoplanmomo"
+  location            = "westeurope"
+  resource_group_name = "tform-sabdulrahman"
+
+  sku {
+    tier = "Standard"
+  
+    size = "S1"
+  }
+}
+
+
+
+
+resource "azurerm_app_service" "web_app_1" {
+  name                = "cocofofowebapp"
+  location            = "westeurope"
+  resource_group_name = "tform-sabdulrahman"
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+}
